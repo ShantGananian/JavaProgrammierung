36ff2da9aaa5934efa259e90f14c64c079c37bcf
Shant <shant_ga@hotmail.com>
2023-08-16 21:06:02 +0200

    added Grundrechenarten

---
+++ sehr leicht/Mathematische Aufgaben/Grundrechenarten/src/main/java/Grundrechenarten.java
@@ -0,0 +1,23 @@
+/*
+ * Grundrechenarten
+ */
+
+/**
+ *
+ * @author Shant Gananian
+ */
+public class Grundrechenarten {
+
+    public static void main(String[] args) {
+        calc(6, 7);
+        calc(3, 4);
+        calc(5, 5);
+
+    }
+
+    public static int calc(int m, int n) {
+        // System.out.println("Das Resultat für die Zahlen " + m + " und " + n + " ist: " + ((m * n / 2) % 7));
+        return (m * n / 2) % 7;
+
+    }
+}
